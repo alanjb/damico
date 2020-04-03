b3fa4231624317b618d5b11e7331fbf754863f62
Alan <boyce.alan15@gmail.com>
2020-04-02 21:54:21 -0400

    basic scene set up

---
+++ api/scene.js
@@ -0,0 +1,36 @@
+var createScene = function() {
+    // Create the scene space
+    var scene = new BABYLON.Scene(engine);
+  
+    // Add a camera to the scene and attach it to the canvas
+    var camera = new BABYLON.ArcRotateCamera(
+      "Camera",
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      BABYLON.Vector3.Zero(),
+      scene
+    );
+    camera.attachControl(canvas, true);
+  
+    // Add lights to the scene
+    var light1 = new BABYLON.HemisphericLight(
+      "light1",
+      new BABYLON.Vector3(1, 1, 0),
+      scene
+    );
+    var light2 = new BABYLON.PointLight(
+      "light2",
+      new BABYLON.Vector3(0, 1, -1),
+      scene
+    );
+  
+    // This is where you create and manipulate meshes
+    var sphere = BABYLON.MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2 },
+      scene
+    );
+  
+    return scene;
+  };
